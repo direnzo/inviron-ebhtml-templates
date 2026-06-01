6d84df63e3784f5c5962fcde39f5ac7e56ca6d35
templates <du@direnzo.com.br>
2026-06-01 15:01:12 -0300

    fix climatempo layout

--- previsao_climatempo/js/master.js
+++ previsao_climatempo/js/master.js
@@ -71,17 +71,27 @@ window.onload = function () {
         var config = { duration: 10000 };
         var item = loader.data("D_CLIMA_CLIMATEMPO");
         var cidade = "";
-        // C1 contem os 3 dias: D1=hoje, D2=amanha, D3=depois
-        var dias = ["C1_D1_DATAARRAY", "C1_D2_DATAARRAY", "C1_D3_DATAARRAY"];
+        // Busca em todos os arrays disponiveis (C1, C2, C3 com D1, D2, D3)
+        var todosArrays = [
+          "C1_D1_DATAARRAY", "C1_D2_DATAARRAY", "C1_D3_DATAARRAY",
+          "C2_D1_DATAARRAY", "C2_D2_DATAARRAY", "C2_D3_DATAARRAY",
+          "C3_D1_DATAARRAY", "C3_D2_DATAARRAY", "C3_D3_DATAARRAY"
+        ];
+        var agoraTimestamp = Math.floor(new Date().getTime() / 1000);
         var agoraDate = new Date();
+        // Data de hoje (sem hora) para comparacao
+        var hojeDia = agoraDate.getFullYear() + "-" + ("0" + (agoraDate.getMonth() + 1)).slice(-2) + "-" + ("0" + agoraDate.getDate()).slice(-2);
         var horaAlvo = agoraDate.getHours();
         var minAlvo = agoraDate.getMinutes();
-        for (var d = 0; d < dias.length; d++) {
+        
+        // Coleta TODOS os registros de todos os arrays
+        var todosFuturos = [];
+        for (var a = 0; a < todosArrays.length; a++) {
           var arr = [];
           if (item && typeof item.value === "function") {
             var arrStr = "";
             try {
-              arrStr = item.value(dias[d]).value;
+              arrStr = item.value(todosArrays[a]).value;
               if (arrStr) {
                 arr = JSON.parse(arrStr);
               }
@@ -91,19 +101,106 @@ window.onload = function () {
           if (cidade === "" && arr.length > 0 && arr[0].city && arr[0].city.ds_name_cit) {
             cidade = arr[0].city.ds_name_cit;
           }
-          // Busca o registro mais proximo do horario alvo (hora/minuto do momento)
+          // Adiciona registros cujo DIA seja >= hoje (ignora hora) - USA LOCAL TIME
+          for (var i = 0; i < arr.length; i++) {
+            var dt = new Date(arr[i].dt_date_wea * 1000);
+            var diaReg = dt.getFullYear() + "-" + ("0" + (dt.getMonth() + 1)).slice(-2) + "-" + ("0" + dt.getDate()).slice(-2);
+            if (diaReg >= hojeDia) {
+              arr[i]._sourceArray = todosArrays[a]; // debug
+              todosFuturos.push(arr[i]);
+            }
+          }
+        }
+        
+        // Agrupa registros futuros por DIA (ignorando hora) - USA LOCAL TIME
+        var diasMap = {}; // { "2026-06-01": [reg1, reg2, ...], "2026-06-02": [...] }
+        for (var i = 0; i < todosFuturos.length; i++) {
+          var reg = todosFuturos[i];
+          var dt = new Date(reg.dt_date_wea * 1000);
+          var diaKey = dt.getFullYear() + "-" + ("0" + (dt.getMonth() + 1)).slice(-2) + "-" + ("0" + dt.getDate()).slice(-2);
+          if (!diasMap[diaKey]) diasMap[diaKey] = [];
+          diasMap[diaKey].push(reg);
+        }
+        
+        console.log('[CLIMA] Total de registros futuros coletados: ' + todosFuturos.length);
+        
+        // Log detalhado: todos os timestamps agrupados por dia - USA LOCAL TIME (São Paulo)
+        var diasDebug = {};
+        for (var i = 0; i < todosFuturos.length; i++) {
+          var reg = todosFuturos[i];
+          var dt = new Date(reg.dt_date_wea * 1000);
+          var diaKey = dt.getFullYear() + "-" + ("0" + (dt.getMonth() + 1)).slice(-2) + "-" + ("0" + dt.getDate()).slice(-2);
+          if (!diasDebug[diaKey]) diasDebug[diaKey] = [];
+          diasDebug[diaKey].push(dt.getHours() + 'h');
+        }
+        console.log('[CLIMA] Detalhamento por dia:');
+        for (var dk in diasDebug) {
+          if (diasDebug.hasOwnProperty(dk)) {
+            console.log('  ' + dk + ': ' + diasDebug[dk].length + ' registros - horários: ' + diasDebug[dk].join(', '));
+          }
+        }
+        
+        // Log de todos os timestamps (primeiros e ultimos de cada array)
+        console.log('[CLIMA] Timestamps por array:');
+        for (var a = 0; a < todosArrays.length; a++) {
+          var arrNome = todosArrays[a];
+          var registrosArray = [];
+          for (var i = 0; i < todosFuturos.length; i++) {
+            if (todosFuturos[i]._sourceArray === arrNome) {
+              registrosArray.push(todosFuturos[i]);
+            }
+          }
+          if (registrosArray.length > 0) {
+            var primeiro = registrosArray[0];
+            var ultimo = registrosArray[registrosArray.length - 1];
+            var dtPrimeiro = new Date(primeiro.dt_date_wea * 1000);
+            var dtUltimo = new Date(ultimo.dt_date_wea * 1000);
+            console.log('  ' + arrNome + ': ' + registrosArray.length + ' registros | ' +
+              dtPrimeiro.toLocaleString('pt-BR') + ' até ' +
+              dtUltimo.toLocaleString('pt-BR'));
+          }
+        }
+        
+        // Funcao auxiliar: busca registro mais proximo do horario alvo - USA LOCAL TIME
+        function buscarMelhorHorario(registrosDia, horaAlvo, minAlvo) {
           var melhorReg = null;
           var menorDiff = 999999999;
           var alvoSeg = horaAlvo * 3600 + minAlvo * 60;
-          for (var i = 0; i < arr.length; i++) {
-            var dataObj = new Date(arr[i].dt_date_wea * 1000);
+          for (var i = 0; i < registrosDia.length; i++) {
+            var dataObj = new Date(registrosDia[i].dt_date_wea * 1000);
             var seg = dataObj.getHours() * 3600 + dataObj.getMinutes() * 60;
             var diff = Math.abs(seg - alvoSeg);
             if (diff < menorDiff) {
               menorDiff = diff;
-              melhorReg = arr[i];
+              melhorReg = registrosDia[i];
             }
           }
+          return melhorReg;
+        }
+        
+        // Gera os 3 dias esperados: hoje, amanha, depois de amanha
+        var diasEsperados = [];
+        for (var offset = 0; offset < 3; offset++) {
+          var dataEsperada = new Date(agoraDate.getTime());
+          dataEsperada.setDate(dataEsperada.getDate() + offset);
+          var diaKey = dataEsperada.getFullYear() + "-" + ("0" + (dataEsperada.getMonth() + 1)).slice(-2) + "-" + ("0" + dataEsperada.getDate()).slice(-2);
+          diasEsperados.push(diaKey);
+        }
+        
+        console.log('[CLIMA] Dias esperados: ' + diasEsperados.join(', '));
+        console.log('[CLIMA] Dias disponiveis no XML: ' + Object.keys(diasMap).sort().join(', '));
+        
+        // Busca registro para cada dia esperado
+        for (var d = 0; d < diasEsperados.length; d++) {
+          var diaKey = diasEsperados[d];
+          var registrosDia = diasMap[diaKey];
+          
+          if (!registrosDia || registrosDia.length === 0) {
+            console.warn('[CLIMA] ALERTA: Sem dados para o dia ' + diaKey + ' (Card ' + (d + 1) + ')');
+            continue;
+          }
+          
+          var melhorReg = buscarMelhorHorario(registrosDia, horaAlvo, minAlvo);
           if (melhorReg) {
             var dataObj = new Date(melhorReg.dt_date_wea * 1000);
             var diaSemana = ["DOMINGO", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA", "SÁBADO"][dataObj.getDay()];
@@ -112,6 +209,8 @@ window.onload = function () {
             var mes = dataObj.getMonth() + 1;
             if (mes < 10) mes = "0" + mes;
             var dataStr = dia + "/" + mes;
+            // Log do registro selecionado
+            console.log('[CLIMA] Card ' + (d + 1) + ' | array=' + (melhorReg._sourceArray || 'N/A') + ' | data=' + dataStr + ' ' + ("0" + dataObj.getHours()).slice(-2) + ":" + ("0" + dataObj.getMinutes()).slice(-2) + ' | ts=' + melhorReg.dt_date_wea + ' | icon=' + melhorReg.nr_icon_wea + ' | max=' + melhorReg.nr_max_wea + ' min=' + melhorReg.nr_min_wea + ' | prob_chuva=' + melhorReg.nr_probrain_wea + ' | vento=' + melhorReg.ds_winddirection_wea + ' ' + melhorReg.nr_windavgvelocity_wea + 'km/h | uv=' + melhorReg.nr_uv_wea);
             dados.push({
               CIDADE: cidade,
               DIA: diaSemana,
@@ -153,18 +252,37 @@ function iniciarTemplate(dados, config, loader) {
   }
   document.body.style.setProperty("--clima-principal", corClima);
 
-  // Preencher cards estáticos
-  var cards = document.querySelectorAll(".card");
-  for (var i = 0; i < cards.length && i < dados.length; i++) {
-    preencherCardClima(cards[i], dados[i], corClima);
+  // Criar cards dinamicamente a partir do template
+  var cardsContainer = document.getElementById("cards");
+  var template = document.getElementById("card-template");
+  var cardsElements = [];
+  
+  for (var i = 0; i < dados.length; i++) {
+    // Clona o template
+    var cardClone = template.content.cloneNode(true);
+    var card = cardClone.querySelector(".card");
+    
+    // Preenche o card com dados
+    preencherCardClima(card, dados[i], corClima);
+    
+    // Adiciona no container
+    cardsContainer.appendChild(card);
+    cardsElements.push(card);
   }
+
+  console.log('[CLIMA] Cards renderizados: ' + cardsElements.length);
 
   // Fade in do body
   document.body.classList.remove("opacity-0");
   document.body.classList.add("opacity-100");
 
-  // Só anima depois de preencher os dados
-  animarCards(cards, dados.length);
+  // Força reflow antes de animar (para garantir que animação funcione)
+  void cardsContainer.offsetHeight;
+  
+  // Aguarda próximo frame para aplicar animação
+  setTimeout(function() {
+    animarCards(cardsElements, dados.length);
+  }, 50);
 
   if (loader) loader.loaded();
   setTimeout(function () {
