fe0f845f5df2568a65ca9f471589948bab49caad
templates <du@direnzo.com.br>
2026-09-11 10:38:23 -0300

    fix rodape midiah

--- rodape_midiah/js/config.js
+++ rodape_midiah/js/config.js
@@ -1,10 +1,10 @@
 var CONFIG = {
     fraseFixa: 'SEU SUCESSO COMEÇA COM A ESCOLHA CERTA DE MÍDIA',
     telefone: '(19) 92005-0381',
-    tempoTotalExibicao: 60000,
-    itemDuracao: 5500,
-    fadeDuracao: 350,
-    watchdogTempo: 70000,
+    tempoTotalExibicao: 60000, // 1 minutos. tempo total de exibição em milissegundos
+    itemDuracao: 5500, // duração de cada item em milissegundos
+    fadeDuracao: 350,   // duração do efeito de transição em milissegundos
+    watchdogTempo: 70000,       // tempo de watchdog em milissegundos
     maxIdadeCotacaoMinutos: 180,
     maxIdadeXmlMinutos: 360,
     toleranciaRelogioMinutos: 5,
